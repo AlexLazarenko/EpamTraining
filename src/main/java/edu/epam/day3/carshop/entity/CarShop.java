279890abe7de7f7000d9b07de9ba8783169050a7
AlexLazarenko <lazarenko.aa89@gmail.com>
2020-11-19 17:24:40 +0300

    fix

--- src/main/java/edu/epam/day3/carshop/entity/CarShop.java
+++ src/main/java/edu/epam/day3/carshop/entity/CarShop.java
@@ -19,6 +19,21 @@ public class CarShop {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CarShop carShop = (CarShop) o;
+
+        return cars != null ? cars.equals(carShop.cars) : carShop.cars == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return cars != null ? cars.hashCode() : 0;
+    }
+
+    @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("CarShop{");
         sb.append("cars=").append(cars);
